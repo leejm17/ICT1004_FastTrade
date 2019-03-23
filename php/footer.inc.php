0a7d6f7f9411399dc26861e61943af8a8272aedc
JM Lee <lee_jm0117@hotmail.com>
2019-03-23 22:11:08 +0800

    Uploaded: navbar & footer.inc.php
    NOTE FOLDERS for which css and inc.php reside at.

---
+++ php/footer.inc.php
@@ -0,0 +1,73 @@
+<?php
+
+echo
+'<footer class="nk-footer" style="background-color: #f2f2f2;">
+    
+
+    <div class="nk-footer-widgets text-gray">
+        <div class="container">
+                <div class="row vertical-gap">
+                <div class="col-lg-4">
+                    <div class="nk-widget">
+                        <h4 class="nk-widget-title text-dark">Our Headquarters</h4>
+                        <p class="mb-7">
+                            10 Dover Street, <br>
+                            Singapore 138683
+                        </p>
+                        <p class="mb-7">
+                            (+65) 6800 5500
+                        </p>
+                        <p>
+                            customer@fasttrade.com
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="nk-widget">
+                        <h4 class="nk-widget-title text-dark">Development Team</h4>
+                        <p class="mb-7">
+                            172 Ang Mo Kio Avenue 8, <br>
+                            Singapore 567739
+                        </p>
+                        <p class="mb-7">
+                            (+65) 6800 4400
+                        </p>
+                        <p>
+                            technical@fasttrade.com
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="nk-widget">
+                        <h4 class="nk-widget-title text-dark">Follow Us</h4>
+                        <div class="nk-social-2 text-gray">
+                            <ul>
+                                <li><a class="nk-social-facebook" href="https://www.facebook.com/"><span><span class="nk-social-front">Facebook</span><span class="nk-social-back">Facebook</span></span></a></li>
+                                <li><a class="nk-social-pinterest" href="https://pinterest.com/"><span><span class="nk-social-front">Pinterest</span><span class="nk-social-back">Pinterest</span></span></a></li>
+                                <li><a class="nk-social-instagram" href="https://www.instagram.com/"><span><span class="nk-social-front">Instagram</span><span class="nk-social-back">Instagram</span></span></a></li>
+                                <li><a class="nk-social-google" href="https://plus.google.com/"><span><span class="nk-social-front">Google Plus</span><span class="nk-social-back">Google Plus</span></span></a></li>
+                                <li><a class="nk-social-twitter" href="https://twitter.com/"><span><span class="nk-social-front">Twitter</span><span class="nk-social-back">Twitter</span></span></a></li>
+                                <li><a class="nk-social-linkedin" href="https://linkedin.com/"><span><span class="nk-social-front">LinkedIn</span><span class="nk-social-back">LinkedIn</span></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="nk-footer-cont nk-footer-cont-sm">
+        
+            <a class="nk-footer-scroll-top-btn nk-anchor  text-dark" href="#top">
+                <span class="pe-7s-angle-up"></span>
+            </a>
+        
+        <div class="container text-center">
+            <div class="nk-footer-text text-gray">
+                <p>2019 &copy; Design by Team02. Code by nK</p>
+            </div>
+        </div>
+    </div>
+</footer>'
+
+?>
---
+++ php/navbar.inc.php
@@ -0,0 +1,199 @@
+<?php
+
+echo
+'<header class="nk-header nk-header-left nk-header-opaque">
+
+    
+        <!--
+            START: Navbar
+
+            Additional Classes:
+                .nk-navbar-dark
+        -->
+        <nav class="nk-navbar nk-navbar-cont nk-navbar-dark d-none d-lg-flex">
+            
+            <a href="index.php" class="nk-nav-logo">
+                <img src="assets/images/logo-2.svg" alt="" width="19" class="nk-nav-logo-img-dark">
+                <img src="assets/images/logo-2-light.svg" alt="" width="19" class="nk-nav-logo-img-light">
+            </a>
+            
+            <a href="#" class="nk-navbar-left-toggle">
+                <span class="nk-icon-burger">
+                    <span class="nk-t-1"></span>
+                    <span class="nk-t-2"></span>
+                    <span class="nk-t-3"></span>
+                </span>
+            </a>
+            <div class="nk-social">
+                <ul>
+                    <li><a class="nk-social-facebook" href="https://www.facebook.com/"><span><span class="nk-social-front fa fa-facebook"></span><span class="nk-social-back fa fa-facebook"></span></span></a></li>
+                    <li><a class="nk-social-instagram" href="https://instagram.com/"><span><span class="nk-social-front fa fa-instagram"></span><span class="nk-social-back fa fa-instagram"></span></span></a></li>
+                    <li><a class="nk-social-twitter" href="https://twitter.com/"><span><span class="nk-social-front fa fa-twitter"></span><span class="nk-social-back fa fa-twitter"></span></span></a></li>
+                    <li><a class="nk-social-pinterest" href="https://pinterest.com/"><span><span class="nk-social-front fa fa-pinterest"></span><span class="nk-social-back fa fa-pinterest"></span></span></a></li>
+                    <li><a class="nk-social-google" href="https://plus.google.com/"><span><span class="nk-social-front fa fa-google"></span><span class="nk-social-back fa fa-google"></span></span></a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- END: Navbar -->
+
+        <!--
+            START: Navbar Left
+
+            Additional Classes:
+                .nk-navbar-lg
+                .nk-navbar-overlay-content
+                .nk-navbar-dark
+                .nk-navbar-items-effect-1
+                .nk-navbar-drop-effect-1
+                .nk-navbar-drop-effect-2
+        -->
+        <div class="nk-navbar nk-navbar-left nk-navbar-lg nk-navbar-overlay-content nk-navbar-dark nk-navbar-drop-effect-2 d-none d-lg-block">
+            <div class="nano">
+                <div class="nano-content">
+                    <div class="nk-nav-table">
+                        <div class="nk-nav-row nk-nav-row-full nk-nav-row-center">
+                            <ul class="nk-nav" data-nav-mobile="#nk-nav-mobile">
+                                
+        <li class="active">
+            <a href="index.php">
+                Shop
+                
+            </a>
+        </li>
+        <li>
+            <a href="product.php">
+                Product
+                
+            </a>
+        </li>
+<!--        <li>
+            <a href="shop-cart.html">
+                Cart
+                
+            </a>
+        </li>-->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END: Navbar Left -->
+        <!--
+            START: Navbar
+
+            Will be shown on small screens
+
+            Additional Classes:
+                .nk-navbar-lg
+                .nk-navbar-sticky
+                .nk-navbar-autohide
+                .nk-navbar-transparent
+                .nk-navbar-transparent-always
+                .nk-navbar-white-text-on-top
+                .nk-navbar-dark
+        -->
+        <nav class="nk-navbar nk-navbar-top nk-navbar-dark d-lg-none">
+            <div class="container">
+                <div class="nk-nav-table">
+                    
+                    <a href="index.php" class="nk-nav-logo">
+                        <img src="assets/images/logo-2.svg" alt="" width="19" class="nk-nav-logo-img-dark">
+                        <img src="assets/images/logo-2-light.svg" alt="" width="19" class="nk-nav-logo-img-light">
+                    </a>
+                    
+                    <ul class="nk-nav nk-nav-right nk-nav-icons">
+                        
+                            <li class="single-icon">
+                                <a href="#" class="nk-navbar-full-toggle">
+                                    <span class="nk-icon-burger">
+                                        <span class="nk-t-1"></span>
+                                        <span class="nk-t-2"></span>
+                                        <span class="nk-t-3"></span>
+                                    </span>
+                                </a>
+                            </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- END: Navbar -->
+    
+
+</header>
+<!-- END: Nav Header -->
+
+    
+    
+    
+        <!--
+    START: Navbar Mobile
+
+    Additional Classes:
+        .nk-navbar-dark
+        .nk-navbar-align-center
+        .nk-navbar-align-right
+        .nk-navbar-items-effect-1
+        .nk-navbar-drop-effect-1
+        .nk-navbar-drop-effect-2
+-->
+<nav class="nk-navbar nk-navbar-full nk-navbar-align-center nk-navbar-dark nk-navbar-drop-effect-1" id="nk-nav-mobile">
+    
+        <div class="nk-navbar-bg">
+            <div class="bg-image">
+                <img src="assets/images/bg-menu-full.jpg" alt="" class="jarallax-img">
+            </div>
+        </div>
+    
+    <div class="nk-nav-table">
+        <div class="nk-nav-row">
+            <div class="container">
+                <div class="nk-nav-header">
+                    
+                    <div class="nk-nav-logo">
+                        <a href="index.php" class="nk-nav-logo">
+                            
+                                <img src="assets/images/logo-2.svg" alt="" width="19" class="nk-nav-logo-img-dark">
+                                <img src="assets/images/logo-2-light.svg" alt="" width="19" class="nk-nav-logo-img-light">
+                            
+                        </a>
+                    </div>
+                    
+                    <div class="nk-nav-close nk-navbar-full-toggle">
+                        <span class="nk-icon-close"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nk-nav-row-full nk-nav-row">
+            <div class="nano">
+                <div class="nano-content">
+                    <div class="nk-nav-table">
+                        <div class="nk-nav-row nk-nav-row-full nk-nav-row-center nk-navbar-mobile-content">
+                            <ul class="nk-nav">
+                                <!-- Here will be inserted menu from [data-mobile-menu="#nk-nav-mobile"] -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nk-nav-row">
+            <div class="container">
+                <div class="nk-social">
+                    <ul>
+                        <li><a class="nk-social-facebook" href="https://www.facebook.com/"><span><span class="nk-social-front fa fa-facebook"></span><span class="nk-social-back fa fa-facebook"></span></span></a></li>
+                        <li><a class="nk-social-instagram" href="https://instagram.com/"><span><span class="nk-social-front fa fa-instagram"></span><span class="nk-social-back fa fa-instagram"></span></span></a></li>
+                        <li><a class="nk-social-twitter" href="https://twitter.com/"><span><span class="nk-social-front fa fa-twitter"></span><span class="nk-social-back fa fa-twitter"></span></span></a></li>
+                        <li><a class="nk-social-pinterest" href="https://pinterest.com/"><span><span class="nk-social-front fa fa-pinterest"></span><span class="nk-social-back fa fa-pinterest"></span></span></a></li>
+                        <li><a class="nk-social-google" href="https://plus.google.com/"><span><span class="nk-social-front fa fa-google"></span><span class="nk-social-back fa fa-google"></span></span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+<!-- END: Navbar Mobile -->'
+
+?>
