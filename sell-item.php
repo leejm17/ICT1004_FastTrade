6d195bfee0fb8311da17ad198545976443c3c325
oldmanCH <46952289+oldmanCH@users.noreply.github.com>
2019-03-30 15:25:55 +0800

    Hand over
    Display page:
    Problem left is if user have 2 or 3picture, the item will appear twice or thrice.
    edit button need to redirect to edit-item.php where the page will get the item_id and allow editing

--- sell-item.php
+++ sell-item.php
@@ -60,7 +60,7 @@
 
     <!-- jQuery -->
     <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
     
 </head>
 
